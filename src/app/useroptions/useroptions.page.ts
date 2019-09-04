a68e376f112c228469f603feae68fd9729219ad6
Jordy <alvarezjordi609@gmail.com>
2019-09-04 17:01:29 -0600

    Modifying useroptions view, login form and signup form.

--- src/app/useroptions/useroptions.page.ts
+++ src/app/useroptions/useroptions.page.ts
@@ -1,17 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../servicios/auth.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-useroptions',
   templateUrl: './useroptions.page.html',
   styleUrls: ['./useroptions.page.scss'],
 })
-export class UseroptionsPage {
+export class UseroptionsPage implements OnInit{
+  email: string;
+  password: string;
 
-  constructor(private router:Router) {}
+  constructor(private authservice: AuthService, public router: Router, public alertController: AlertController) { }
 
-  goLogIn(){
-    this.router.navigate(['/login']);
-  } 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'Datos de Usuario',
+      message: 'Los datos ingresados son incorrectos o este usario no se encuentra registrado.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  ngOnInit() {
+  }
+
+  OnSubmitLogin() {
+
+    this.authservice.login(this.email, this.password).then(res => {
+      this.router.navigate(['/home']);
+    }).catch(err => this.presentAlert());
+  }
 
 }
