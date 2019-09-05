3fad5f0576849cd8e77092fe8a18512030b8c84e
Klissman Espinoza <klissman010@gmail.com>
2019-09-04 21:22:11 -0600

    sign up and login are now in a single page

--- src/app/useroptions/useroptions.page.ts
+++ src/app/useroptions/useroptions.page.ts
@@ -9,11 +9,15 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./useroptions.page.scss'],
 })
 export class UseroptionsPage implements OnInit{
+
   email: string;
   password: string;
 
-  constructor(private authservice: AuthService, public router: Router, public alertController: AlertController) { }
+  choosed: Boolean;
+  
 
+  constructor(private authservice: AuthService, public router: Router, public alertController: AlertController) { }
+  
   async presentAlert() {
     const alert = await this.alertController.create({
       header: 'Alerta',
@@ -25,8 +29,85 @@ export class UseroptionsPage implements OnInit{
     await alert.present();
   }
 
-  ngOnInit() {
+  click(str){
+    var sect = document.getElementById("section");
+    if (str == "1") {
+      sect.innerHTML = `<ion-card-header>
+      <ion-card-title>Iniciar Sesión</ion-card-title>
+      <ion-card-subtitle>Ingrese sus datos</ion-card-subtitle>
+    </ion-card-header>
+    <ion-card-content>
+      <ion-card>
+        <ion-card-content>
+          <form action="user" class="center">
+            <ion-item class="">
+              <ion-label position="floating" >Usuario</ion-label>
+              <ion-input type="email" [(ngModel)]="email" name="email"></ion-input>
+            </ion-item>
+            <ion-item class="user">
+              <ion-label position="floating">Contraseña</ion-label>
+              <ion-input [(ngModel)]="password" type="password" name="password"></ion-input>
+            </ion-item>
+          </form>
+          <ion-button (click)="OnSubmitLogin()" class="button">Ingresar</ion-button>
+        </ion-card-content>
+      </ion-card>
+    </ion-card-content>`
+    }else{
+
+      sect.innerHTML = `<ion-card-header>
+        <ion-card-title>Registrarse</ion-card-title>
+        <ion-card-subtitle>Ingrese sus datos para registrarse</ion-card-subtitle>
+      </ion-card-header>
+        <ion-card-content>
+          <ion-card>
+            <ion-card-content>
+              <form action="" class="center">
+                <ion-item class="">
+                  <ion-label position="floating" >Nombre</ion-label>
+                  <ion-input  name="userName"></ion-input>
+                </ion-item>
+                <ion-item class="">
+                  <ion-label position="floating" >Apellido</ion-label>
+                  <ion-input  name="userSurname"></ion-input>
+                </ion-item>
+                <ion-item class="">
+                  <ion-label position="floating" >Edad</ion-label>
+                  <ion-input  type="number" name="userAge" min=0 ></ion-input>
+                </ion-item>
+                <ion-item class="">
+                  <ion-label position="floating" >Correo</ion-label>
+                  <ion-input  type="email" name="userEmail"></ion-input>
+                </ion-item>
+                <ion-item class="">
+                  <ion-label position="floating" >País</ion-label>
+                  <ion-input  name="userCountry"></ion-input>
+                </ion-item>
+                <ion-item class="">
+                  <ion-label position="floating" >Departamento</ion-label>
+                  <ion-input  name="userDepart"></ion-input>
+                </ion-item>
+                <ion-item class="user">
+                  <ion-label position="floating">Contraseña</ion-label>
+                  <ion-input type="password" name="userPassword"></ion-input>
+                </ion-item>
+                <ion-item class="user">
+                  <ion-label position="floating">Confirmar Contraseña</ion-label>
+                  <ion-input type="password" name="userPassword2"></ion-input>
+                </ion-item>
+              </form>
+              <ion-button class="button">Registrarse</ion-button>
+            </ion-card-content>
+          </ion-card>
+        </ion-card-content>`
+    }
   }
+
+
+  ngOnInit() {   
+  }
+
+
 
   OnSubmitLogin() {
 
